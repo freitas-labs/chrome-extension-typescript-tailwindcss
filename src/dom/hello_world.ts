dae4578254dd2a9468a6f1a82f421fa3c7abc00e
freitzzz <1160907@isep.ipp.pt>
2023-06-09 14:34:14 +0100

    feat: init

---
+++ src/dom/hello_world.ts
@@ -0,0 +1,6 @@
+export default function () {
+    const helloWorldElement = document.createElement('h1');
+    helloWorldElement.innerText = 'Hello world!';
+
+    return helloWorldElement;
+}
---
+++ src/dom/index.ts
@@ -0,0 +1,2 @@
+export { default as HelloWorld } from './hello_world';
+export { default as Sum } from './sum';
